b0a1cb63fc862c495ed85f403f137d44d5430290
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-17 10:58:14 +0630

    -

---
+++ DotNet8WebApi.DinkToPdfExample/Services/IPDFService.cs
@@ -0,0 +1,10 @@
+﻿using DotNet8WebApi.DinkToPdfExample.Models;
+
+namespace DotNet8WebApi.DinkToPdfExample.Services
+{
+    public interface IPDFService
+    {
+        public Task<string> GetHtml(UserModel user);
+        public Task<byte[]> GeneratePdf()l+;
+    }
+}
