6e7a8f1faae356663e9d835de2a0d47cb1855fc8
Linn Thit Htoo <lth1212001@gmail.com>
2024-08-17 11:45:43 +0630

    [ Complete ]

---
+++ DotNet8WebApi.DinkToPdfExample/Controllers/UserController.cs
@@ -0,0 +1,45 @@
+﻿using DinkToPdf.Contracts;
+using DotNet8WebApi.DinkToPdfExample.Models;
+using DotNet8WebApi.DinkToPdfExample.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection.Metadata;
+
+namespace DotNet8WebApi.DinkToPdfExample.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IPDFService _pdfService;
+
+        public UserController(IPDFService pdfService)
+        {
+            _pdfService = pdfService;
+        }
+
+        [HttpPost("Generate-PDF")]
+        public async Task<IActionResult> GeneratePdf()
+        {
+            try
+            {
+                var user = new UserModel
+                {
+                    UserId = 1,
+                    UserName = "Linn Thit",
+                    UserRole = "Admin",
+                    IsActive = true
+                };
+
+                var htmlStr = await _pdfService.GetHtml(user);
+                var pdf = await _pdfService.GeneratePdf(htmlStr);
+
+                return File(pdf, "application/pdf", $"{user.UserName}.pdf");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+    }
+}
--- DotNet8WebApi.DinkToPdfExample/Program.cs
+++ DotNet8WebApi.DinkToPdfExample/Program.cs
@@ -1,8 +1,20 @@
+using DinkToPdf.Contracts;
+using DinkToPdf;
+using DotNet8WebApi.DinkToPdfExample.Services;
+using DotNet8WebApi.DinkToPdfExample;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton(typeof(IConverter), new SynchronizedConverter(new PdfTools()));
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+var wkHtmlToPdfPath = Path.Combine(Directory.GetCurrentDirectory(), $"libs\\");
+
+CustomAssemblyLoadContext context = new();
+context.LoadUnmanagedLibrary(Path.Combine(wkHtmlToPdfPath, "libwkhtmltox.dll"));
+builder.Services.AddScoped<IPDFService, PDFService>();
 
 var app = builder.Build();
 
